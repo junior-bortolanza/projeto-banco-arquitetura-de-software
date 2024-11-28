7fec11fd2d2f5ce6d03567576b72fb3df27bdafa
Junior Bortolanza <gbortolanzajr@gmail.com>
2024-11-28 13:47:54 -0300

    feat: Updating pessoa_juridica in repository interface

--- src/models/sqlite/interfaces/cliente_pessoa_juridica_repository.py
+++ src/models/sqlite/interfaces/cliente_pessoa_juridica_repository.py
@@ -3,9 +3,24 @@ from abc import ABC, abstractmethod
 class ClientePessoaJuridicaInteface(ABC):
 
     @abstractmethod
+    def create_pessoa_juridica(self, 
+                    faturamento: float, 
+                    idade: int, 
+                    nome_fantasia: str, 
+                    celular: str, 
+                    email_corporativo: str, 
+                    categoria: str, 
+                    saldo: float 
+                ) -> None:
+        pass
+
+    @abstractmethod
     def sacar_dinheiro(self, quantia: float, pessoa_juridica: str) -> None:
         pass
 
     @abstractmethod
-    def consulta_saldo(self, pessoa_juridica: str) -> dict: 
+    def consultar_saldo(self, pessoa_juridica: str) -> dict: 
+        pass
+    
+    def list_pessoa_juridica(self) -> list:
         pass
