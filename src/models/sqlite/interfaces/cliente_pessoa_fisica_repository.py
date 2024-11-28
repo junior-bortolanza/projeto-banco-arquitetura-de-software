6ffbb9b052b84001f2bf9c7e6712e1e692d62203
Junior Bortolanza <gbortolanzajr@gmail.com>
2024-11-28 13:46:23 -0300

    feat: Updating interface pessoa fisica

--- src/models/sqlite/interfaces/cliente_pessoa_fisica_repository.py
+++ src/models/sqlite/interfaces/cliente_pessoa_fisica_repository.py
@@ -3,9 +3,25 @@ from abc import ABC, abstractmethod
 class ClientePessoaFisicaInterface(ABC):
 
     @abstractmethod
+    def create_pessoa_fisica(
+                self, 
+                renda_mensal: str, 
+                idade: int, 
+                nome_completo: str,
+                celular: str, 
+                email: str, 
+                categoria: str, 
+                saldo
+            ) -> None:
+        pass
+
+    @abstractmethod
     def sacar_dinheiro(self, quantia: float, pessoa_fisica: str) -> None:
         pass
 
     @abstractmethod    
     def consultar_saldo(self, pessoa_fisica: str) -> dict: 
+        pass
+
+    def list_pessoa_fisica(self) -> list:
         pass
